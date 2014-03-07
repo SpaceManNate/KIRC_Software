1c884a57eeb4bc4d56bba5112ba6da9f6ac6522a
Nathaniel <Nathaniel@10.32.240.198>
2014-03-07 10:06:33 -0500

    update_v1.1

--- KIRC_FltCmp_v0.1/Debug/configPkg/package/cfg/KIRC_FltCmp_RTOS_pem4f.cfg.mak
+++
@@ -1,8 +0,0 @@
-# invoke SourceDir generated makefile for KIRC_FltCmp_RTOS.pem4f
-KIRC_FltCmp_RTOS.pem4f: .libraries,KIRC_FltCmp_RTOS.pem4f
-.libraries,KIRC_FltCmp_RTOS.pem4f: package/cfg/KIRC_FltCmp_RTOS_pem4f.xdl
-	$(MAKE) -f C:\Users\Nathaniel\workspace_v5_5\KIRC_Software\KIRC_FltCmp_v0.1/src/makefile.libs
-
-clean::
-	$(MAKE) -f C:\Users\Nathaniel\workspace_v5_5\KIRC_Software\KIRC_FltCmp_v0.1/src/makefile.libs clean
-
--- KIRC_FltCmp_v0.1/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"libc.a" -l"C:/ti/tirtos_1_20_00_28/products/TivaWare_C_Series-2.0b/driverlib/ccs/Debug/driverlib.lib" -l"C:/ti/tirtos_1_20_00_28/products/TivaWare_C_Series-2.0b/usblib/ccs/Debug/usblib.lib"
-
